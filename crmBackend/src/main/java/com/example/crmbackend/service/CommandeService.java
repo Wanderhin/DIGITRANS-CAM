40aa4c3dd4d9ad255e0b08d5408a2445e970790f
Gouaffo johann <jgouaffo@gmail.com>
2026-05-22 07:00:44 +0200

    crud commande

--- crmBackend/src/main/java/com/example/crmbackend/service/CommandeService.java
+++ crmBackend/src/main/java/com/example/crmbackend/service/CommandeService.java
@@ -5,7 +5,7 @@ import com.example.crmbackend.dto.response.CommandeResponse;
 import com.example.crmbackend.entity.Commande;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
+//
 public interface CommandeService {
     Page<CommandeResponse> list(Commande.StatutCommande statut, Pageable pageable);
     CommandeResponse getById(Long id);
